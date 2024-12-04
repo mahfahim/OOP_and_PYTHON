134893eb70834e6fad8b71697cdbd4b72bac2ac9
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-12-04 19:10:55 +0600

    alll

---
+++ 0_Practice/practice_6_5/1.py
@@ -0,0 +1,12 @@
+# Create a Product class and a Shop class.
+# Inside the Shop class, create a method name add_product which adds products using the Product class to the Shop class.
+# Inside the Shop class, create a method name buy_product which is used to buy a product and 
+# check whether this product is available or not. If you successfully buy a product, then throw a Congress message.
+# What is Inheritance? Explain with examples
+# What are Encapsulation and Access Modifiers? Explain with examples
+
+class product:
+    pass
+
+class Shop:
+    pass
